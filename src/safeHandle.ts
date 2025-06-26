454fc375c292e5805a555219257944085b18e0fa
Lora Hristova <lorimira__hristova@abv.bg>
2025-06-26 14:25:09 +0300

    feat: adding handle shadow

--- src/safeHandle.ts
+++ src/safeHandle.ts
@@ -1,10 +1,23 @@
-import { Sprite } from '@pixi/sprite';
+import { Container } from '@pixi/display';
 import { Texture } from '@pixi/core';
+import { Sprite } from '@pixi/sprite';
 
-export class SafeHandle extends Sprite {
+export class SafeHandle extends Container {
+    private mainSprite: Sprite;
+    private shadowSprite: Sprite;
+
     constructor() {
-        super(Texture.from('images/handle.png'));
+        super();
 
-        this.anchor.set(0.5, 0.5);
+        this.mainSprite = new Sprite(Texture.from('images/handle.png'));
+        this.mainSprite.anchor.set(0.5, 0.5);
+
+        this.shadowSprite = new Sprite(Texture.from('images/handleShadow.png'));
+        this.shadowSprite.anchor.set(0.5, 0.5);
+        this.shadowSprite.x = 20;
+        this.shadowSprite.y = 20;
+
+        this.addChild(this.shadowSprite);
+        this.addChild(this.mainSprite);
     }
 }
